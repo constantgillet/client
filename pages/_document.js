69a061862fe4567f56d0669a0e9fd736d49a2ec6
constantgillet <constant.gillet@hetic.net>
2021-10-27 23:40:24 +0200

    Add gtm and remove google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -16,25 +16,14 @@ export default class MyDocument extends Document {
             }}
           ></script>
           {/* <!-- End Google Tag Manager --> */}
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `
-            }}
-          />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
