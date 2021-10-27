710c18d736ba8092b7c9deb9adb72663aa1f167f
constantgillet <constant.gillet@hetic.net>
2021-10-27 23:55:01 +0200

    Add gtm

--- pages/_document.js
+++ pages/_document.js
@@ -18,12 +18,14 @@ export default class MyDocument extends Document {
           {/* <!-- End Google Tag Manager --> */}
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-            }}
-          ></noscript>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
